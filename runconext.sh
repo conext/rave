a6ffe2c8b77193027c353102566e97f14a8c93d8
Lukasz Moren <lukasz.moren@gmail.com>
2013-04-26 14:21:21 +0200

    autodeploy file fix

--- runconext.sh
+++ runconext.sh
@@ -2,6 +2,6 @@
 sh /home/apps/conext-bamboo/conext/bin/shutdown.sh
 sleep 5
 rm -rf /home/apps/conext-bamboo/conext
-tar -xvzf /home/apps/conext-bamboo/conext.tar.gz
+tar -xvzf /home/apps/conext-bamboo/conext-bin.tar.gz
 sh /home/apps/conext-bamboo/conext/bin/startup.sh
 exit 0
