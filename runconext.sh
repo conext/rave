31e5289864af6e19c72e8d176e1d8c6a3d57ae37
Lukasz Moren <lukasz.moren@gmail.com>
2013-04-26 13:26:41 +0200

    test fix and script for autodeploying

---
+++ runconext.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sh /home/apps/conext-bamboo/apache-rave-0.22-SNAPSHOT-bin/bin/shutdown.sh
+sleep 5
+rm -rf /home/apps/conext-bamboo/apache-rave-0.22-SNAPSHOT-bin
+tar -xvzf /home/apps/conext-bamboo/*.tar -d /home/apps/conext-bamboo/
+sh /home/apps/conext-bamboo/apache-rave-0.22-SNAPSHOT-bin/bin/startup.sh
+exit 0
