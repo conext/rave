cd53f8f0bd4cc3e8c3c56dc1343d19b3ed006cfa
Lukasz Moren <lukasz.moren@gmail.com>
2013-05-07 10:25:09 +0200

    - run deployment scripts in the folder of SSH user

--- runconext.sh
+++ runconext.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo sh /home/apps/conext-bamboo/conext/bin/shutdown.sh
+sh /home/lukasz/conext-bamboo/conext/bin/shutdown.sh
 sleep 5
-sudo rm -rf /home/apps/conext-bamboo/conext
-sudo tar -zxvf /home/apps/conext-bamboo/conext-bin.tar.gz -C /home/apps/conext-bamboo/
-sudo sh /home/apps/conext-bamboo/conext/bin/startup.sh
+rm -rf /home/lukasz/conext-bamboo/conext
+tar -zxvf /home/lukasz/conext-bamboo/conext-bin.tar.gz -C /home/apps/conext-bamboo/
+sh /home/lukasz/conext-bamboo/conext/bin/startup.sh
 exit 0
