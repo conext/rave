c7957163f9644ef96f1872122c02e944ed913975
Lukasz Moren <lukasz.moren@gmail.com>
2013-05-21 14:54:28 +0200

    CONEXT-98 Reconfigure Bamboo builds to use "deploy" user for automatic Conext redeployment

--- rave-portal-resources/src/main/dist/bin/catalina.sh
+++ rave-portal-resources/src/main/dist/bin/catalina.sh
@@ -87,7 +87,7 @@
 # OS specific support.  $var _must_ be set to either true or false.
 
 
-CATALINA_OPTS="-Xmx1024m -XX:MaxPermSize=256m DEBUG_CONFIG -Dportal.override.properties="classpath:PROPERTIES_PROFILE-portal.properties" -Drave-shindig.override.properties="classpath:PROPERTIES_PROFILE-rave.shindig.properties""
+CATALINA_OPTS="-Xmx1024m -XX:MaxPermSize=256m -Xdebug -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5012 -Dportal.override.properties="classpath:ci-portal.properties" -Drave-shindig.override.properties="classpath:ci-rave.shindig.properties""
 
 cygwin=false
 os400=false
