a6f31350e2a1b1a4c9510cb56a19e0218df32dba
Lukasz Moren <lukasz.moren@gmail.com>
2013-05-22 15:51:54 +0200

    - wait a bit longer for server shutdown

--- runconext.sh
+++ runconext.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sh /home/deploy/conext-bamboo/conext/bin/catalina.sh stop
-sleep 10
+sleep 20
 rm -rf /home/deploy/conext-bamboo/conext
 tar -zxvf /home/deploy/conext-bamboo/conext-bin.tar.gz -C /home/deploy/conext-bamboo/
 sh /home/deploy/conext-bamboo/conext/bin/startup.sh
